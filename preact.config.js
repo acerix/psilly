16417faf415b2a99932a69485f952220a994d8cf
acerix <dylan@psilly.com>
2021-06-22 02:07:09 -0400

    Fix relative URLs

---
+++ preact.config.js
@@ -0,0 +1,8 @@
+export default (config, env, helpers) => {
+    config.output.publicPath = './'
+    config.plugins.push(
+    new helpers.webpack.DefinePlugin({
+        'process.env.PUBLIC_PATH': JSON.stringify(config.output.publicPath || '/')
+    })
+    )
+}
