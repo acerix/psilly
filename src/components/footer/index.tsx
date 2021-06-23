bfc0f072f898504972444add2ead94da614a041a
acerix <dylan@psilly.com>
2021-06-23 02:36:56 -0400

    Fix footer responsiveness

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,10 +4,16 @@ const Footer: FunctionalComponent = () => {
   return (
     <footer class="footer text-muted py-3 mt-5">
       <div class="container">
-        <p class="float-end mb-1">
-          <a target="_blank" rel="noreferrer" class="text-muted text-decoration-none" href="https://github.com/acerix/psilly">&lt;sauce/&gt;</a>
-        </p>
-        <p class="mb-1">© <time id="copyright-year">{new Date().getFullYear()}</time> <a class="text-muted text-decoration-none" href="https://psilly.com/">Psilly Corporation</a>. Psilly™ is a trademark of <a class="text-muted text-decoration-none" href="https://psilly.com/">Psilly Corporation</a>. All Rights Reserved.</p>
+        <div class="row">
+          <div class="col-sm-8 col-md-10">
+            <p class="mb-1">© <time id="copyright-year">{new Date().getFullYear()}</time> <a class="text-muted text-decoration-none" href="https://psilly.com/">Psilly Corporation</a>. Psilly™ is a trademark of <a class="text-muted text-decoration-none" href="https://psilly.com/">Psilly Corporation</a>. All Rights Reserved.</p>
+          </div>
+          <div class="col-sm-4 col-md-2">
+            <p class="float-end mb-1">
+              <a target="_blank" rel="noreferrer" class="text-muted text-decoration-none" href="https://github.com/acerix/psilly">&lt;sauce/&gt;</a>
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   )
