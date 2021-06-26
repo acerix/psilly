0f1c593b50ffd73fc97096ddd067db1d23568793
acerix <dylan@psilly.com>
2021-06-26 02:13:25 -0400

    Updates

---
+++ route-highness/index.tsx.chunk.9246d.esm.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"u+W7":function(n,e,s){"use strict";s.r(e);var t=s("hosL"),u=s("Hrl7");e.default=n=>{const{highness:e}=n;return Object(t.h)("section",{class:"container py-5"},Object(t.h)(u.a,{title:"Your Highness"}),Object(t.h)("h1",null,"Your Highness"),"high"===e&&Object(t.h)("p",null,"Hi High, I'm Dud."),Object(t.h)("p",null,"I understand you identify as ",Object(t.h)("em",null,e)," and I fully support you in this endeavor."))}}}]);
+//# sourceMappingURL=index.tsx.chunk.9246d.esm.js.map
