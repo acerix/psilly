125109789815051d5ff5e28280440a8e2a6218f1
acerix <dylan@psilly.com>
2021-06-26 15:36:41 -0400

    Updates

--- route-not-found/index.tsx.chunk.a8fa3.esm.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{"9/y1":function(o,e,t){"use strict";t.r(e);var n=t("hosL"),c=t("Hrl7"),r=t("ox/y");e.default=()=>Object(n.h)("section",{class:"container py-5"},Object(n.h)(c.a,{title:"Page Not Found"}),Object(n.h)("h1",null,"Page Not Found — Error 404"),Object(n.h)("p",null,"Sorry, we couldn't find the page you're looking for."),Object(n.h)(r.Link,{href:"/"},"Back to Home"))}}]);
-//# sourceMappingURL=index.tsx.chunk.a8fa3.esm.js.map
