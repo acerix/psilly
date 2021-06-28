5e425b6737ae212d30fc709281a431ad69a5d614
acerix <dylan@psilly.com>
2021-06-28 00:56:03 -0400

    Updates

--- route-chat.chunk.d3cb5.esm.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{liLl:function(e,t,n){"use strict";n.r(t);var l=n("hosL"),a=n("Hrl7");t.default=()=>Object(l.h)("section",{class:"container py-5"},Object(l.h)(a.a,{title:"Chatter"}),Object(l.h)("h1",null,"Psilly Chatter Next Generation"),Object(l.h)("p",null,"Looks like you found the new version of Psilly Chatter! It's coming soon, I swear!"),Object(l.h)("p",null,Object(l.h)("a",{href:"/experiments/chatter.pill",target:"_blank"},"Legacy Chatter")))}}]);
-//# sourceMappingURL=route-chat.chunk.d3cb5.esm.js.map
