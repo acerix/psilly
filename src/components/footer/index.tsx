10b6b9abecd79c66c1d628f5bd87cbbfad109aa7
acerix <dylan@psilly.com>
2021-07-04 02:09:37 -0400

    Add yt

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,14 +5,19 @@ const Footer: FunctionalComponent = () => {
     <footer class="footer text-muted py-3 mt-5">
       <div class="container">
         <div class="row">
-          <div class="col-sm-8 col-md-10">
-            <p class="mb-1">© <time id="copyright-year">{new Date().getFullYear()}</time> <a class="text-muted text-decoration-none" href="https://psilly.com/">Psilly Corporation</a>. Psilly™ is a trademark of <a class="text-muted text-decoration-none" href="https://psilly.com/">Psilly Corporation</a>. All Rights Reserved.</p>
+          <div class="col-sm-9 col-md-10">
+            <p class="mb-2">© <time id="copyright-year">{new Date().getFullYear()}</time> <a class="text-muted text-decoration-none" href="https://psilly.com/">Psilly Corporation</a>. Psilly™ is a trademark of <a class="text-muted text-decoration-none" href="https://psilly.com/">Psilly Corporation</a>. All Rights Reserved.</p>
           </div>
-          <div class="col-sm-4 col-md-2">
-            <p class="float-end mb-1">
+          <div class="col-sm-5 col-md-2">
+            <p class="float-end mb-2">
               <a target="_blank" rel="noreferrer" class="text-muted text-decoration-none" href="https://github.com/acerix/psilly">&lt;sauce/&gt;</a>
             </p>
           </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="float-end">
+          <a class="text-muted text-decoration-none" href="https://www.youtube.com/channel/UCNYwYcc65kItw98wcqU0-Gg" target="_blank" rel="noreferrer">YouTube</a>
         </div>
       </div>
     </footer>
