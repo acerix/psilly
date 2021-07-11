9bbfc28bb669d94ca0bd67250679fb5e70622e27
acerix <dylan@psilly.com>
2021-07-10 20:02:35 -0400

    Starting to look hilberty

--- src/routes/art/chillbert/index.tsx
+++ src/routes/art/chillbert/index.tsx
@@ -6,58 +6,35 @@ import {ArtPlaque, Artwork} from '../meta'
 import artworkLibrary from '../library'
 import style from './style.css'
 
-// function hindex2xy(hindex: number, N: number) {
-//   const positions = [
-//   /* 0: */ [0, 0],
-//     /* 1: */ [0, 1],
-//     /* 2: */ [1, 1],
-//     /* 3: */ [1, 0]
-//   ]
+type wtfIsTPL = {
+  [key:string]: Array<[number,number,string]>;
+}
 
-//   const tmp = positions[last2bits(hindex)]
-//   let t = 0
-//   hindex = (hindex >>> 2)
+const TPL: wtfIsTPL = {
+  R: [[0, 1, 'U'], [1, 1, 'R'], [1, 0, 'R'], [0, 0, 'D']],
+  L: [[1, 0, 'D'], [0, 0, 'L'], [0, 1, 'L'], [1, 1, 'U']],
+  D: [[1, 0, 'L'], [1, 1, 'D'], [0, 1, 'D'], [0, 0, 'R']],
+  U: [[0, 1, 'R'], [0, 0, 'U'], [1, 0, 'U'], [1, 1, 'L']]
+}
 
-//   let x = tmp[0]
-//   let y = tmp[1]
-
-//   for (let n = 4; n <= N; n *= 2) {
-//     const n2 = n / 2
-
-//     switch (last2bits(hindex)) {
-//     case 0: /* left-bottom */
-//       t = x; x = y; y = t
-//       break
-
-//     case 1: /* left-upper */
-//       // x = x
-//       y = y + n2
-//       break
-
-//     case 2: /* right-upper */
-//       x = x + n2
-//       y = y + n2
-//       break
-
-//     case 3: /* right-bottom */
-//       t = y
-//       y = (n2-1) - x
-//       x = (n2-1) - t
-//       x = x + n2
-//       break
-//     }
-
-//     hindex = (hindex >>> 2)
-//   }
-
-//   return [x, y]
-
-//   function last2bits(x: number) { return (x & 3) }
-// }
+const hilbert = (rank: number, size: number, dx = 0, dy = 0, dir = 'U'): number[] => {
+  if (rank === 0) {
+    return TPL[dir].map(p => [dx + p[0] * size, dy + p[1] * size])
+  } 
+  const arr: number[] = []  
+  const space = size / (Math.pow(2, rank + 1) - 1)
+  const newSize = (size - space) / 2
+  const d2 = newSize + space
+  TPL[dir].forEach((t) => {
+    [].push.apply(arr, hilbert(rank - 1, newSize, dx + t[0] * d2, dy + t[1] * d2, t[2]))
+  })
+  return arr
+}
 
 class ChillbertSnake {
-  position = new Int16Array([0, 0])
-  direction = 0
+  position = 0
+  negative = Math.random() < 0.5
+  speed = Math.ceil(Math.random() * 5)
   colorPeriods: number[] = [randomColorPeriod(), randomColorPeriod(), randomColorPeriod()]
   colorGenerator = ColorGenerator({
     mutate: (color: Color, iterationCount: number): void => {
@@ -69,21 +46,21 @@ class ChillbertSnake {
 }
 
 const Chillbert: FunctionalComponent = () => {
-  const maxSnakes = 32
-  const snakeLength = 16
   const snakes: ChillbertSnake[] = []
-  // let hilbertSize = 4
+  let hilbertSpace: number[] = []
+  const hilbertRank = 8
   
   const init = (ctx: CanvasRenderingContext2D): void => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.lineWidth = 8
+    // ctx.lineWidth = 2
+    const maxSnakes = Math.floor(Math.sqrt(ctx.canvas.width * ctx.canvas.height) / 16)
     while (maxSnakes > snakes.length) {
       snakes.push(new ChillbertSnake())
     }
+    const spaceSize = Math.max(ctx.canvas.width, ctx.canvas.height)
+    hilbertSpace = hilbert(hilbertRank, spaceSize, 1, 1)
     for (const snake of snakes) {
-      snake.position[0] = Math.floor(Math.random() * ctx.canvas.width)
-      snake.position[1] = Math.floor(Math.random() * ctx.canvas.height)
-      snake.direction = Math.floor(Math.random() * 4)
+      snake.position = Math.floor(Math.random() * hilbertSpace.length)
     }
   }
 
@@ -91,24 +68,11 @@ const Chillbert: FunctionalComponent = () => {
     for (const snake of snakes) {
       ctx.strokeStyle = colorToCss(snake.colorGenerator.next().value as Color)
       ctx.beginPath()
-      ctx.moveTo(snake.position[0], snake.position[1])
-      const [x1, y1] = snake.position
-      if (snake.direction===0) snake.position[1] -= snakeLength
-      else if (snake.direction===1) snake.position[0] += snakeLength
-      else if (snake.direction===2) snake.position[1] += snakeLength
-      else if (snake.direction===3) snake.position[0] -= snakeLength
-      snake.direction = Math.floor(Math.random() * 4)
-      const [w, h] = [ctx.canvas.width+snakeLength, ctx.canvas.height+snakeLength]
-      if (snake.position[0] < 0) snake.position[0] += w
-      if (snake.position[1] < 0) snake.position[1] += h
-      snake.position[0] %= w
-      snake.position[1] %= h
-      const [x2, y2] = snake.position
-      // don't draw snakes teleporting across
-      if (Math.abs(x1%w - x2) > snakeLength || Math.abs(y1%h - y2) > snakeLength) {
-        continue
+      ctx.moveTo(...hilbertSpace[snake.position])
+      for (let i=0; i<snake.speed; i++) {
+        snake.position = (snake.position + (snake.negative ? -1 : 1)) % hilbertSpace.length
+        ctx.lineTo(...hilbertSpace[snake.position])
       }
-      ctx.lineTo(snake.position[0], snake.position[1])
       ctx.stroke()
     }
   }
