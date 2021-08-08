7cdd7e64db12e79f1fc1d007f206f7b36a7b7527
acerix <dylan@psilly.com>
2021-08-08 01:15:17 -0400

    Met eh?

--- src/routes/art/meta.tsx
+++ src/routes/art/meta.tsx
@@ -26,7 +26,7 @@ export const ArtPlaque: FunctionalComponent<ArtPlaqueProps> = (props: ArtPlaqueP
   art.thumbnailUrl = `/assets/art/thumbnails/${art.path}.webp`
   return <div class="card art_plaque" ref={ref}>
     <a href={art.displayUrl} aria-label="Experience">
-      <img src={art.thumbnailUrl} width={300} height={185} class="card-img-top" alt="View" />
+      <img src={art.thumbnailUrl} class="card-img-top" alt="Preview" />
     </a>
     <div class="card-body">
       <h5 class="card-title">
