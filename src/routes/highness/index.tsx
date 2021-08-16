6f640bdfb9e5706b33787ad2dc28a332b081bdb5
acerix <dylan@psilly.com>
2021-08-16 12:06:57 -0400

    Add tripsit link

--- src/routes/highness/index.tsx
+++ src/routes/highness/index.tsx
@@ -15,7 +15,7 @@ const Highness: FunctionalComponent<HighnessProps> = (props: HighnessProps) => {
       <p>I understand you identify as <em>{highness}</em> and fully support you in this endeavor.</p>
       {highness==='sober' && <div><p>If you could use some help, try <span style="color:black;background:black;">love</span>.</p></div>}
       {highness==='high' && <div><p>If you want to get higher, ask yourself "<span style="color:black;background:black;">what is higher?</span>".</p></div>}
-      {highness==='too high' && <div><p>If you could use some help, try this:</p><audio controls><source src="/help.flac" type="audio/flac"><a href="/help.flac" target="_blank">Help!</a></source></audio></div>}
+      {highness==='too high' && <div><audio controls><source src="/help.flac" type="audio/flac"><a href="/help.flac" target="_blank">Help!</a></source></audio><p>A slow, calm chat may help you relax, try <a href="https://chat.tripsit.me/chat/?a=1#sanctuary" target="_blank" rel="noreferrer">TripSit</a>.</p></div>}
     </section>
   )
 }
