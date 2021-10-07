1da6f3b087d133d250f4cd474b86c2f77230fab4
acerix <dylan@psilly.com>
2021-10-07 03:18:35 -0400

    Updates

--- route-not-found.chunk.bbf78.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{M6X6:function(o,t,n){"use strict";n.r(t);var e=n("hosL"),r=n("Hrl7"),a=n("ox/y");t.default=function(){return Object(e.h)("section",{class:"container py-5"},Object(e.h)(r.a,{title:"Page Not Found"}),Object(e.h)("h1",null,"Page Not Found — Error 404"),Object(e.h)("p",null,"Sorry, the thing you are looking for is not a thing."),Object(e.h)(a.Link,{href:"/"},"Country roads, take me home"))}}}]);
-//# sourceMappingURL=route-not-found.chunk.bbf78.js.map
