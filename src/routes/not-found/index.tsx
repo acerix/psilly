439fb133d52005775887456afde87e19c6740dc3
acerix <dylan@psilly.com>
2021-10-07 03:04:16 -0400

    psilosophize

--- src/routes/not-found/index.tsx
+++ src/routes/not-found/index.tsx
@@ -6,7 +6,7 @@ const NotFound: FunctionalComponent = () => {
   return (
     <section class="container py-5">
       <Helmet title="Page Not Found" />
-      <h1>Page Not Found — Error 404</h1>
+      <h1>Thing Not Found — Error 404</h1>
       <p>Sorry, the thing you are looking for is not a thing.</p>
       <Link href="/">
           Country roads, take me home
