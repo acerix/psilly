8dfe58df038726283f2c9f7c122fb2632c88fc47
acerix <dylan@psilly.com>
2021-10-11 01:22:08 -0400

    speed

--- src/routes/art/lemnisgo/fragment.js
+++ src/routes/art/lemnisgo/fragment.js
@@ -11,14 +11,16 @@ uniform vec2 u_scale;
 out vec4 fragmentColor;
 
 void main() {
-  float m_0 = ( gl_FragCoord.x + u_translate.x ) * u_scale.x;
-  float m_1 = ( gl_FragCoord.y + u_translate.y ) * u_scale.y;
+  float x = ( gl_FragCoord.x + u_translate.x ) * u_scale.x;
+  float y = ( gl_FragCoord.y + u_translate.y ) * u_scale.y;
+  float x2 = x*x;
+  float y2 = y*y;
 
   // lemniscate of bernoulli (0 = -2x² + x⁴ + 2y² + 2x²y² + y⁴)
-  float z = -2.0*m_0*m_0 + m_0*m_0*m_0*m_0 + 2.0*m_1*m_1 + 2.0*m_0*m_0*m_1*m_1 + m_1*m_1*m_1*m_1;
+  float z = -2.0*x2 + x2*x2 + 2.0*y2 + 2.0*x2*y2 + y2*y2;
 
+  fragmentColor.r = mod(z + u_time / 43.0, 1.0);
   fragmentColor.g = z < 1.0 ? z : 1.0 / z;
-  fragmentColor.r = mod(z + u_time * 0.007, 1.0);
-  fragmentColor.b = mod(z - u_time * 0.011, 1.0);
+  fragmentColor.b = mod(z - u_time / 73.0, 1.0);
 }
 `
