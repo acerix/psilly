16309400519f952011fae8c04e850de3fb1e3ff7
acerix <dylan@psilly.com>
2021-11-04 18:57:49 -0400

    Updates

--- route-spinner.chunk.6f4ef.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{pvpb:function(e,n,t){"use strict";t.r(n);var i=t("hosL"),s=t("Hrl7"),c="spinner__35ncf";n.default=function(){return Object(i.h)("section",{class:c},Object(i.h)(s.a,{title:"Please wait while your desires are loaded..."}),Object(i.h)("div",null,Object(i.h)("div",null,"⬡")))}}}]);
-//# sourceMappingURL=route-spinner.chunk.6f4ef.js.map
