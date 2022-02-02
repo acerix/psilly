6fa9f0f3952c0724c93e4b4529937776c37333c8
Dylan Ferris <dylan@psilly.com>
2022-02-01 21:04:28 -0500

    Updates

--- route-art.chunk.1b5ed.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{gZd1:function(e,t,n){"use strict";n.r(t);var r=n("hosL"),c=n("Hrl7"),i=n("sRXP");t.default=function(){return Object(r.h)("section",{class:"container py-5"},Object(r.h)(c.a,{title:"Web Art Exhibition"}),Object(r.h)("h1",null,"Web Art"),Object(r.h)(i.a,null),Object(r.h)("h2",null,"Artchive"),Object(r.h)("p",null,"Older art experiments are under ",Object(r.h)("a",{href:"/experiments/"},"experiments"),"."))}}}]);
-//# sourceMappingURL=route-art.chunk.1b5ed.js.map
