eb3961e9c33d2c50dc7198310dfab7e5a7e42fa0
Dylan Ferris <dylan@psilly.com>
2022-03-28 21:55:00 -0400

    Split webgl effects

--- src/routes/art/common/webgl2/index.tsx
+++ src/routes/art/common/webgl2/index.tsx
@@ -34,14 +34,62 @@ const defaultContextOptions = {
 
 export const WebGL2: FunctionalComponent<WebGL2Props> = (props: WebGL2Props) => {
   const { getContext, init, ready, draw, onResize, framesPerSecond, ...rest } = props
-  const ref = createRef()
   const frameMilliseconds = framesPerSecond ? 1000 / framesPerSecond : undefined
+  const ref = createRef()
+  let paused = false
+  let frame = 0
   
+  // Pause animation when window is not focused
+  useEffect(() => {
+    const handleBlur = (): void => {
+      paused = true
+    }
+    window.addEventListener('blur', handleBlur)
+    const handleFocus = (): void => {
+      paused = false
+    }
+    window.addEventListener('focus', handleFocus)
+    return (): void => {
+      window.removeEventListener('blur', handleBlur)
+      window.removeEventListener('focus', handleFocus)
+    }
+  }, [])
+
+  // Update canvas dimensions when window is resized
   useEffect(() => {
     const canvas = ref.current as HTMLCanvasElement
-    let paused = false
-    let frameCount = -1
-    let renderCallbackID: number
+    const ctx = getContext ? getContext(canvas) : canvas.getContext('webgl2', defaultContextOptions) as WebGL2RenderingContext
+    const handleResize = (): void => {
+      ctx.canvas.width = window.innerWidth
+      ctx.canvas.height = window.innerHeight
+      if (onResize) onResize(ctx)
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return (): void => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [ref])
+
+  // Set fullscreen on click
+  useEffect(() => {
+    const setFullscreen = (): void => {
+      if (!document.fullscreenElement) {
+        document.body.requestFullscreen().catch(err => {
+          console.error('Fullscreen fail', err)
+        })
+      }
+    }
+    window.addEventListener('click', setFullscreen)
+    return (): void => {
+      window.removeEventListener('click', setFullscreen)
+    }
+  }, [ref])
+
+  // Start render loop
+  useEffect(() => {
+    const canvas = ref.current as HTMLCanvasElement
+    let loopCallbackID: number
 
     const ctx = getContext ? getContext(canvas) : canvas.getContext('webgl2', defaultContextOptions) as WebGL2RenderingContext
     if (!ctx) {
@@ -52,50 +100,23 @@ export const WebGL2: FunctionalComponent<WebGL2Props> = (props: WebGL2Props) => 
 
     if (init) init(ctx)
 
-    const handleResize = (): void => {
-      ctx.canvas.width = window.innerWidth
-      ctx.canvas.height = window.innerHeight
-      if (onResize) onResize(ctx)
-    }
-    window.addEventListener('resize', handleResize)
-    handleResize()
-
-    const handleBlur = (): void => {
-      paused = true
-    }
-    window.addEventListener('blur', handleBlur)
-
-    const handleFocus = (): void => {
-      paused = false
-    }
-    window.addEventListener('focus', handleFocus)
-
-    const setFullscreen = (): void => {
-      if (!document.fullscreenElement) {
-        document.body.requestFullscreen().catch(err => {
-          console.error('Fullscreen fail:', err)
-        })
-      }
-    }
-
-    window.addEventListener('click', setFullscreen)
     const render = (): void => {
       if (paused) {
-        setTimeout(render, 128)
+        loopCallbackID = window.setTimeout(render, 128)
         return
       }
-      frameCount++
+      frame++
       if (frameMilliseconds) {
-        renderCallbackID = window.setTimeout(render, frameMilliseconds)
+        loopCallbackID = window.setTimeout(render, frameMilliseconds)
       }
       else {
-        renderCallbackID = window.requestAnimationFrame(render)
+        loopCallbackID = window.requestAnimationFrame(render)
       }
-      draw(ctx, frameCount)
+      draw(ctx, frame)
     }
 
     const whenReady = (): void => {
-      setTimeout(render, 0)
+      loopCallbackID = window.setTimeout(render, 0)
     }
 
     if (ready===undefined) whenReady()
@@ -103,18 +124,14 @@ export const WebGL2: FunctionalComponent<WebGL2Props> = (props: WebGL2Props) => 
 
     return (): void => {
       if (frameMilliseconds) {
-        window.clearTimeout(renderCallbackID)
+        window.clearTimeout(loopCallbackID)
       }
       else {
-        window.cancelAnimationFrame(renderCallbackID)
+        window.cancelAnimationFrame(loopCallbackID)
       }
-      window.removeEventListener('resize', handleResize)
-      window.removeEventListener('blur', handleBlur)
-      window.removeEventListener('focus', handleFocus)
-      window.removeEventListener('click', setFullscreen)
     }
 
-  }, [getContext, init, ready, draw, onResize, ref, frameMilliseconds])
+  }, [ref])
 
   return <canvas ref={ref} {...rest} />
 }
