39c92b33587c4fa88f8e69ad1d68932aed6ecdb0
Dylan Ferris <dylan@psilly.com>
2022-04-09 17:54:31 -0400

    Updates

--- route-not-found.chunk.06fc5.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{M6X6:function(o,t,n){"use strict";n.r(t);var e=n("hosL"),r=n("Hrl7"),a=n("JBDq");t.default=function(){return Object(e.g)("section",{class:"container py-5"},Object(e.g)(r.a,{title:"Page Not Found"}),Object(e.g)("h1",null,"Thing Not Found — Error 404"),Object(e.g)("p",null,"Sorry, the thing you are looking for is not a thing."),Object(e.g)(a.a,{href:"/"},"Country roads, take me home"))}}}]);
-//# sourceMappingURL=route-not-found.chunk.06fc5.js.map
