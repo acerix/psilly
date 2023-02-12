a3977ec856d78d3d07fae5785ddeb7f2f8d53adf
Dylan Ferris <dylan@psilly.com>
2023-02-12 16:47:45 -0500

    Updates

---
+++ pages/art/common/bézier-curve/index.tsx
@@ -0,0 +1,9 @@
+export class Point {
+  position: number[] = [0, 0]
+}
+
+class BézierCurve {
+  points: Point[] = []
+}
+
+export default BézierCurve
---
+++ pages/art/falls/vertex.js
@@ -0,0 +1,11 @@
+export default `
+precision lowp float;
+attribute vec2 vertexCoords;
+uniform mat3 coordinateTransform;
+uniform float pointSize;
+void main() {
+    vec3 transformedCoords = coordinateTransform * vec3(vertexCoords, 1.0);
+    gl_Position = vec4(transformedCoords.xy, 0.0, 1.0);
+    gl_PointSize = pointSize;
+}
+`
